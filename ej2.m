44b6462ad0f5c3a5a29fdfeaa8b2ef30d8f4b0de
Frank Emilio Perez Rivero <frank90cs@gmail.com>
2015-01-18 18:03:43 +0100

    Solved Q-2-2

--- ej2.m
+++ ej2.m
@@ -1,4 +1,6 @@
 pkg load image
+
+%% Ej 2-1
 I = imread("I3T_200_WM_orig.png")
 BW = im2bw(I, graythresh(I));
 [L,NL]=	bwlabel(BW)
@@ -15,10 +17,18 @@ for it=1:length(labels)
 endfor
 
 BWM1 = zeros(size(I))
-BWM1(maxBlob) = I(maxBlob)
+BWM1(maxBlob) = BW(maxBlob)
+BWM1 = im2bw(BWM1, graythresh(BWM1))
+figure,imshow(BWM1)
 
-figure,imshow(BWM1)
-figure,imshow(BWM1)
+%% Ej 2-2
+BW3 = bwmorph(BWM1,'skel', Inf)
+figure,imshow(BW3)
+
+BW4 = bwmorph(BWM1,'remove')
+figure,imshow(BW4)
+figure,imshow(BW3)
+
 
 input('Press any key to continue')
 
